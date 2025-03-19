985d51ff44922bc9fbde0923c811c0a02cf50578
Radoshnov Mihail <tenchu047@gmail.com>
2025-03-19 09:46:18 +0300

    add js

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 let a  = 10;
 function fun(par){
 let b = 23;
-return par + b   
+return par + b;  
 }
 
 console.log(fun(a));
+
+let obj = {}
+
+console.log(obj)
+
+
+console.log()
+
+
+console.log(1 && 0);
+
+
+if(1 && 0){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
