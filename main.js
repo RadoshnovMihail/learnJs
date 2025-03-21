05908efadd8f707e1fd119fb9439288b89d2f534
Radoshnov Mihail <tenchu047@gmail.com>
2025-03-21 20:00:17 +0300

    add js

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-let a  = 10;
-function fun(par){
-let b = 23;
-return par + b;  
+const newPost  = (post, addedAt = Date()) => ({
+    ...post,
+    addedAt
+})
+
+const firstPost = {
+    id: 1,
+    author: 'Misha'
 }
 
-console.log(fun(a));
-
-let obj = {}
-
-console.log(obj)
-
-
-console.log()
-
-
-console.log(1 && 0);
-
-
-if(1 && 0){
-    console.log('true');
-}
-else{
-    console.log('false');
-}
+console.log(newPost(firstPost))
