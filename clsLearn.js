288b8008de9da7fb543a80e2e5d6e841d4a48aa7
Radoshnov Mihail <tenchu047@gmail.com>
2025-03-24 12:18:43 +0300

    learn class

--- clsLearn.js
+++ clsLearn.js
@@ -1,26 +1,59 @@
-class Comment {
-    constructor(text) {
-        this.text = text
-        this.votesQty = 0
+// class Comment {
+//     constructor(text){
+//     this.text = text;
+//     this.votesQty = 0;        
+//     }
+//     upvote(){
+//         this.votesQty++;
+//     }
+// }
+
+// const firstComment = new Comment('This is my first comment');
+
+// firstComment instanceof Comment; // true
+// firstComment.Object instanceof Object; // 
+
+// class Student {
+//     planet = 'Earth'
+//     coutnry = 'USA'
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     logAge(){
+//         console.log(this.age)
+//     }
+// }
+
+// const firstStudent = new Student('John', 25);
+// const secondStudent = new Student('Jane', 30);
+
+// console.log(firstStudent)
+// secondStudent.logAge()
+
+
+class Student {
+    #city = null
+    constructor(name, age){
+        this.name = name;
+        this.age = age; 
     }
-    upvote() {
-        this.votesQty += 1
-    }
+   set city(value) {
+    const firstLetter = value[0].toUpperCase()
+    const fromSecondLetter = value.slice(1).toLowerCase()
+
+    this.#city = `${firstLetter}${fromSecondLetter}`
+   }
+   get city(){
+    return `г. ${this.#city}`
+   }
 }
 
+const firstStudent = new Student('Вася', 25)
 
-const firstComment = new Comment('First comment');
+firstStudent.city = 'москва'
 
-console.log(firstComment);
-
-
-const a = 'abc';
-
-function sum (word){
-    console.log(this);
-}
-
-sum.bind(a);
+console.log(firstStudent.city)
 
 
 
