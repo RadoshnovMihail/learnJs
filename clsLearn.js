3200959b8ef05447c598e2a914dc8379003d970f
Radoshnov Mihail <tenchu047@gmail.com>
2025-03-25 11:40:15 +0300

    learn class

--- clsLearn.js
+++ clsLearn.js
@@ -1,59 +1,19 @@
-// class Comment {
-//     constructor(text){
-//     this.text = text;
-//     this.votesQty = 0;        
-//     }
-//     upvote(){
-//         this.votesQty++;
-//     }
-// }
-
-// const firstComment = new Comment('This is my first comment');
-
-// firstComment instanceof Comment; // true
-// firstComment.Object instanceof Object; // 
-
-// class Student {
-//     planet = 'Earth'
-//     coutnry = 'USA'
-//     constructor(name, age){
-//         this.name = name;
-//         this.age = age;
-//     }
-//     logAge(){
-//         console.log(this.age)
-//     }
-// }
-
-// const firstStudent = new Student('John', 25);
-// const secondStudent = new Student('Jane', 30);
-
-// console.log(firstStudent)
-// secondStudent.logAge()
-
-
-class Student {
-    #city = null
-    constructor(name, age){
-        this.name = name;
-        this.age = age; 
+class User {
+    constructor(name, profession){
+        this.name = name
+        this.profession = profession
     }
-   set city(value) {
-    const firstLetter = value[0].toUpperCase()
-    const fromSecondLetter = value.slice(1).toLowerCase()
 
-    this.#city = `${firstLetter}${fromSecondLetter}`
-   }
-   get city(){
-    return `г. ${this.#city}`
-   }
 }
 
-const firstStudent = new Student('Вася', 25)
+const firstUser = new User('Alex','doctor')
 
-firstStudent.city = 'москва'
+console.log(firstUser)
 
-console.log(firstStudent.city)
+class UserUpdated extends User {
+    accNum(){
+        return this.reduce((acc, el) => acc + el)
+    }
+}
 
-
-
+const arr = new User
