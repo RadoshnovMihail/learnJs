522188d068cd4a5cdb2bba2621c208d1d7ed1169
Radoshnov Mihail <tenchu047@gmail.com>
2025-03-26 12:33:55 +0300

    learn protoype

--- clsLearn.js
+++ clsLearn.js
@@ -1,19 +1,29 @@
-class User {
-    constructor(name, profession){
-        this.name = name
-        this.profession = profession
-    }
+// class NumbersArray extends Array {
+//     sum(){
+//     return   this.reduce((acc, el) => acc + el)
+//     }
+// }
 
-}
+// const myArray = new NumbersArray(2, 4, 5)
 
-const firstUser = new User('Alex','doctor')
+// console.log(myArray)
 
-console.log(firstUser)
+// console.log(myArray.sum())
 
-class UserUpdated extends User {
-    accNum(){
-        return this.reduce((acc, el) => acc + el)
-    }
-}
 
-const arr = new User
+// const animal = { eats: true };
+// const rabbit = { jumps: true };
+
+// rabbit.__proto__ = animal; // rabbit теперь наследует от animal
+
+// console.log(rabbit.eats); // true (из прототипа)
+// console.log(rabbit.jumps); // true (собственное свойство)
+
+
+
+const str = 'abc'
+const myName = new String('Misha')
+
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(myName)
