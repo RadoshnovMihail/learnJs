8bd0be5e1b2a3807e61e6d5f000a0b361ee91a91
Radoshnov Mihail <tenchu047@gmail.com>
2025-05-20 11:40:17 +0300

    CodeWars

---
+++ codewars/validPhoneNumber.js
@@ -0,0 +1,7 @@
+function validPhoneNumber(x){
+    
+}
+
+console.log(validPhoneNumber("(123) 456-7890")) // true
+console.log(validPhoneNumber("(1111)555 2345")) // false
+console.log(validPhoneNumber("(098) 123 4567")) // false
